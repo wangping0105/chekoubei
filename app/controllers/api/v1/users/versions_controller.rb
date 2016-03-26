7a1c4b088cf6089d1a969928950a31a9a68eb4a1
jackping <525399584@qq.com>
2016-03-26 20:26:25 +0800

    skip_before_action :authenticate! in version

--- app/controllers/api/v1/users/versions_controller.rb
+++ app/controllers/api/v1/users/versions_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::Users::VersionsController < Api::V1::BaseController
+  skip_before_action :authenticate!
+
   def lastest_version
     param! :device, String, required: true, in: ['android', 'ios']
 
     app_version = AppVersion.where(app_type: AppVersion::app_types[params[:device]]).last
     app_version = init_version unless app_version.present?
 
+    response.headers['Access-Control-Allow-Origin'] = '*' #跨域问题
     render json: {
       code: 0,
       data: AppVersionSerializer.new(app_version)
