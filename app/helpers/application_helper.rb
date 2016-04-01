5123f229e4a06a3af00752919544253d15bce750
jack ping <525399584@qq.com>
2016-04-01 13:47:45 +0800

    门店介绍 字数限制 100

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def active_by(_controller_name)
     controller_name==_controller_name ? 'active' : ''
   end
+
+  def word_limit(words, length: 100)
+    words.length > length ? "#{words[0..99]}..." : words
+  end
 end
